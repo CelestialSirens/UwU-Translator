19150156b7d539ec5fe8f30d3d06b0297f09c68c
ByrneHub <116261123+ByrneHub@users.noreply.github.com>
2024-03-08 08:55:22 +0000

    First Commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+//
+// Created by User on 08/03/2024.
+//
+
+#include "main.h"
+#include <iostream> //this allows input and output streams
+
+int main() { // The Cinnamonroll Experience
+
+    // NO God did not abandon me. I abdandoned HER OKAY   IT WAS SOMEWHAT MUTUAL THOUGH.
+
+    std::string Love,Heart;
+
+    std::cout << "Hewwo! Wewcome tuwu cinnamonwoww expewience " << std::endl;
+    std::cout << "We onwy speak in uwu, get used tuwu iwt" << std::endl;
+    // I already want to kill myself even more doing this :3
+    std::cin >> Love;
+    std::cout << "Uwu onwy said:" << Love;
+
+
+
+
+    return 0;
+
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by User on 08/03/2024.
+//
+
+#ifndef C_PROJECT_MAIN_H
+#define C_PROJECT_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //C_PROJECT_MAIN_H
